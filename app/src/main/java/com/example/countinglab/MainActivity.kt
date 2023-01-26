700b96c64f0b64c8f0c0ca2e38c3c4cb2d697058
okbWork <okb3@njit.edu>
2023-01-25 20:29:39 -0500

    Finished lab -Added counter -Completed Stretch Goal

--- app/src/main/java/com/example/countinglab/MainActivity.kt
+++ app/src/main/java/com/example/countinglab/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.countinglab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+        val upgradeButton = findViewById<Button>(R.id.buttonSecond)
+        button.setOnClickListener {
+            counter++
+            textView.text = counter.toString()
+            if(counter >= 100) {
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+                    button.text = getString(R.string.Add_2)
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+                }
+            }
     }
 }
